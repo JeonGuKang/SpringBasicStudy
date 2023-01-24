ef0fba4057861992335fb24b9c6cec73661e1cff
JeonGuKang <jgkang@modoric.com>
2023-01-24 21:37:50 +0900

    feat : 스프링 Configuration 싱글톤 테스트

--- src/main/java/com/example/core2/AppConfig.java
+++ src/main/java/com/example/core2/AppConfig.java
@@ -15,18 +15,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService ->
+
     @Bean //스프링 컨테이너에 등록됨
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
